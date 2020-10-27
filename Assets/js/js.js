380a3162a8ed229f1207a43c247a452dbaba60a3
Mohammad Ahmadi <72553527+MohammadAhmadiTHEMO@users.noreply.github.com>
2020-10-27 20:32:17 +0330

    responsive some pages

--- Assets/js/js.js
+++ Assets/js/js.js
@@ -6,3 +6,16 @@ function openNav() {
     document.getElementById("burger-main").style.width = "0";
   }
   
+  function openDropDown(id, id2, id3 ) {
+    let element = document.getElementById(id);
+    document.getElementById(id2).classList.add("dropUp")
+    document.getElementById(id3).classList.add("dropDown")
+    element.classList.add("dropUp");
+  }
+  
+  function closeDropDown(id, id2, id3) {
+    let element = document.getElementById(id);
+    document.getElementById(id2).classList.add("dropDown")
+    document.getElementById(id3).classList.add("dropUp")
+    element.classList.add("dropDown");
+  }
