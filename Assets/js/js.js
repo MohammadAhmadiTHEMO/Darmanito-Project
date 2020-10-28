2abbe84b8af68a4c5ef3343f2fc4c9375fc73f02
Mohammad Ahmadi <72553527+MohammadAhmadiTHEMO@users.noreply.github.com>
2020-10-29 00:50:03 +0330

    Working on responsive

--- Assets/js/js.js
+++ Assets/js/js.js
@@ -6,13 +6,6 @@ function openNav() {
     document.getElementById("burger-main").style.width = "0";
   }
   
-  // function openDropDown(id, id2, id3 ) {
-  //   let element = document.getElementById(id);
-  //   // document.getElementById(id2).classList.add("dropUp")
-  //   // document.getElementById(id3).classList.add("dropDown")
-  //   element.classList.add("dropUp");
-  // }
-  
   function closeDropDown(id, nagative) {
     var x = document.getElementById(id);
     if (x.style.display === "none") {
