dd5ad158e61aca19dcf9f77e4b8af90d4ee769b1
Mohammad Ahmadi <72553527+MohammadAhmadiTHEMO@users.noreply.github.com>
2020-10-28 11:36:59 +0330

    new commit

--- Assets/js/js.js
+++ Assets/js/js.js
@@ -6,16 +6,50 @@ function openNav() {
     document.getElementById("burger-main").style.width = "0";
   }
   
-  function openDropDown(id, id2, id3 ) {
-    let element = document.getElementById(id);
-    document.getElementById(id2).classList.add("dropUp")
-    document.getElementById(id3).classList.add("dropDown")
-    element.classList.add("dropUp");
-  }
+  // function openDropDown(id, id2, id3 ) {
+  //   let element = document.getElementById(id);
+  //   // document.getElementById(id2).classList.add("dropUp")
+  //   // document.getElementById(id3).classList.add("dropDown")
+  //   element.classList.add("dropUp");
+  // }
   
-  function closeDropDown(id, id2, id3) {
-    let element = document.getElementById(id);
-    document.getElementById(id2).classList.add("dropDown")
-    document.getElementById(id3).classList.add("dropUp")
-    element.classList.add("dropDown");
+  function closeDropDown(id, nagative) {
+    var x = document.getElementById(id);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById(nagative).innerHTML = '-'
+    } else {
+      x.style.display = "none";
+      document.getElementById(nagative).innerHTML = '+'
+    }
+  }
+  function closeDropDown1(id, nagative1) {
+    var x = document.getElementById(id);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById(nagative1).innerHTML = '-'
+    } else {
+      x.style.display = "none";
+      document.getElementById(nagative1).innerHTML = '+'
+    }
+  }
+  function closeDropDown2(id, nagative2) {
+    var x = document.getElementById(id);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById(nagative2).innerHTML = '-'
+    } else {
+      x.style.display = "none";
+      document.getElementById(nagative2).innerHTML = '+'
+    }
+  }
+  function closeDropDown3(id, nagative3) {
+    var x = document.getElementById(id);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      document.getElementById(nagative3).innerHTML = '-'
+    } else {
+      x.style.display = "none";
+      document.getElementById(nagative3).innerHTML = '+'
+    }
   }
